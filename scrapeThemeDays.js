77abbdcf346b0f7fc40fef038fa8c8334675a357
niklasHagner <niklas.hagner@outlook.com>
2021-11-22 23:42:57 +0100

    Fix scraper script

--- scrapeThemeDays.js
+++ scrapeThemeDays.js
@@ -1,30 +1,33 @@
-var cheerio = require('cheerio');
+// var cheerio = require('cheerio');
 var request = require("axios");
 
-var data = await request(monthEndpoint);
-var $ = cheerio.load(html);
-cheerio.load($('#container').children('span').eq(0)
+const endPoint = 'https://sholiday.faboul.se/dagar/v2.1' + '/2021'; + 
+request(endPoint).then((data) => {
+    console.log("data", data);
 
+    var $ = cheerio.load(html);
+    cheerio.load($('#container').children('span').eq(0));
 
-var items = [];
-let item = {dayName: "", events: []};
-let dayNameCount = 0;
+    var items = [];
+    let item = {dayName: "", events: []};
+    let dayNameCount = 0;
 
-[...$0.childNodes].forEach((child) => {
-	console.log(child.nodeName, child);
+    [...$0.childNodes].forEach((child) => {
+        console.log(child.nodeName, child);
 
-    if (child.nodeName === "A" ) {
-        item.events.push(child.innerText);
-    }
-    else if (child.nodeName === "#text") {
-        item.events.push(child.textContent);
-    }
-    else if (child.nodeName === "B") {
-        if (item.dayName !== "") {
-            items.push(item);
-            item = {dayName: "", events: []};
+        if (child.nodeName === "A" ) {
+            item.events.push(child.innerText);
         }
-        item.dayName = child.innerText + " 2019";
-    }
+        else if (child.nodeName === "#text") {
+            item.events.push(child.textContent);
+        }
+        else if (child.nodeName === "B") {
+            if (item.dayName !== "") {
+                items.push(item);
+                item = {dayName: "", events: []};
+            }
+            item.dayName = child.innerText + " 2019";
+        }
+    })
+    console.log("items.length", items.length);
 })
-console.log("hehe", items.length);
