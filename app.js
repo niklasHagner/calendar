7c86b9f831ec7307755e1301a1428bb735e3112d
niklasHagner <niklas.hagner@outlook.com>
2022-05-21 18:49:57 +0200

    Correct placement of 1stDayOfMonth

--- app.js
+++ app.js
@@ -43,14 +43,14 @@ const SWEDISH_WEEKDAY_NAMES = [
     'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag'
 ];
 
-
 app.get('/', async function (req, res) { //serve handlebars
     var d = dayjs();
-    var year = d.format('YYYY');
+    var year = Number(d.format('YYYY'));
     var month = d.month() + 1;
-    renderCalendarForMonth(year, month, req, res);
+    renderCalendar(year, month, req, res);
 });
 
+/* This endpoint was intended to be called via links like ?year=2018&month=3*/
 app.get('/showCalendar', async function (req, res) {
     var queryParams = req && req.query;
     if (!queryParams || !queryParams.year || !queryParams.month) {
@@ -61,134 +61,155 @@ app.get('/showCalendar', async function (req, res) {
         year,
         month
     } = queryParams;
-    renderCalendarForMonth(year, month, req, res);
+    renderCalendar(year, month, req, res);
 });
 
-async function renderCalendarForMonth(yearString, monthStringWithLeadingZero, req, res) {
+async function renderCalendar(year, month, req, res) {
     var d = dayjs();
 
-    var year;
-    var month = monthStringWithLeadingZero;
-    var currentRenderingMonthOneIndex = Number(month);
-    var currentRenderingMonthZeroIndex = currentRenderingMonthOneIndex - 1;
-    if (!yearString && !monthStringWithLeadingZero) {
-        year = yearString || d.format('YYYY');
-    } else {
-        year = Number(yearString);
+    const currentTimeObj = {
+        year: year,
+        month: month,
+        monthNumber: month,
+        monthZeroIndex: month -1,
+        dayJsObject: dayjs(`${year}-${month}`)
     }
-    var selectedDate = dayjs(`${year}-${month}`);
+    
+    //Prepare pagination links
+    var nextYearObject = dayjs(`${year+1}-01-01`, "YYYY-MM-DD");
+    // var startOfYear = dayjs(`${year}-01-01`, "YYYY-MM-DD").subtract(1, 'months');
+    var prevMonthMomentObject = dayjs(`${year}-${month}-01`, "YYYY-MM-DD").subtract(1, 'months');
+    var prevDateObj = prevMonthMomentObject;
+    var nextQueryString = `showCalendar?year=${nextYearObject.format('YYYY')}&month=${nextYearObject.format('MM')}`;
+    var prevQueryString = `showCalendar?year=${prevDateObj.format('YYYY')}&month=${prevDateObj.format('MM')}`;
+
+    //Prepare the most necessary data fetch
     var fullApiUrl = `${baseEndpoint}/${year}`;
 
     try {
+        /*  Example api data:
+            dagar:(365) []
+            slutdatum:'2022-12-31'
+            startdatum:'2022-01-01'
+        */
         var response = await request(fullApiUrl);
         data = response.data;
 
         //Extend api-data with themeDays from file
         const themeDayFileRaw = await readFile('./temadagar.json');
-        console.log(themeDayFileRaw);
         const themeDayFile = JSON.parse(themeDayFileRaw);
         const daysScraped = themeDayFile.daysScraped;
 
-        var allDaysOfYear = data.dagar.map((d) => {
+        var allDaysOfYear = data.dagar.map((day) => {
             const matchingThemeDay = daysScraped.find((themeDay) => {
-                return themeDay.date === d.datum;
+                return themeDay.date === day.datum;
             });
-            d.themeDays = (matchingThemeDay) ? matchingThemeDay.themeDays : [];
-            d.themeDays =  d.themeDays.slice(0,2); // GUI only has room for 3 themeDays
+
+            // GUI only has room for 3 themeDays
+            day.themeDays = matchingThemeDay && matchingThemeDay.themeDays.slice(0,2) || [];
 
             //Normalize weekNumber from '01' to '1'
-            if (d.vecka[0] === "0") d.vecka = d.vecka.substr(1, 1);
+            if (day.vecka[0] === "0") day.vecka = day.vecka.substr(1, 1);
 
-            return d;
+            return day;
         });
 
+        //Build monthObjects from dayObjects
         var allMonthObjectsDuringThisYear = [];
         allDaysOfYear.forEach(day => {
-            var currentIterationMonthNumber = Number(day.datum.split("-")[1]);
-            var zeroIndexedMonthNumber = currentIterationMonthNumber -1;
-            var foundObj = allMonthObjectsDuringThisYear.find(m => zeroIndexedMonthNumber == m.zeroIndexedMonthNumber );
+            day.monthNumber = Number(day.datum.split("-")[1]);
+            day.monthZeroIndex = day.monthNumber -1;
+            
+            var foundObj = allMonthObjectsDuringThisYear.find(m => day.monthZeroIndex == m.monthZeroIndex );
             if (foundObj) {
                 foundObj.days.push(day);
             } else {
                 var newMonthObj = {
                     days: [day],
-                    zeroIndexedMonthNumber,
+                    monthZeroIndex: day.monthZeroIndex,
                     monthName: dayjs(day.datum).format("MMMM"),
                     firstDatumOfMonth: day.datum,
                     name: dayjs(day.datum, "YYYY-MM-DD").format('MMMM'),
+                    weekNumbers: getUniqueWeekNumbersForArrayOfDays([day])
                 }
                 allMonthObjectsDuringThisYear.push(newMonthObj);
             }
         });
 
-        allMonthObjectsDuringThisYear.forEach((month) => {
-            month.weekNumbers = getUniqueWeekNumbersForArrayOfDays(month.days);
+        //Separate pass to extend completed monthObjects
+        allMonthObjectsDuringThisYear = allMonthObjectsDuringThisYear.map((monthObj) => {
+            var firstDayName = monthObj.days[0].veckodag;
+            const blankDaysAtStartOfMonth = SWEDISH_WEEKDAY_NAMES.indexOf(firstDayName);
+            for(let i=0; i < blankDaysAtStartOfMonth; i++) {
+                monthObj.days.unshift({isBlank: true});
+            }
+            
+            return {
+                ...monthObj,
+                firstDayNameInMonth: firstDayName,
+                weekNumbers: getUniqueWeekNumbersForArrayOfDays([monthObj.days[0]])
+            }
         });
 
-        days = allDaysOfYear.filter(x => {
-            var monthNumber_oneIndexed = Number(x.datum.split("-")[1]);
-            return monthNumber_oneIndexed === currentRenderingMonthOneIndex;
-        });
 
         allDaysOfYear = getExtendedArrayOfDays(allDaysOfYear);
 
+        const daysOfCurrentMonth = allDaysOfYear.filter(day => day.monthNumber === currentTimeObj.monthNumber);
 
-        // months: {
-        //     number: month,
-        //     name: dayjs(`${year}-${month}-01`, "YYYY-MM-DD").format('MMMM')
-        //     days: daysPerMonth
-        // }
+        const currentMonthObj = allMonthObjectsDuringThisYear.filter(x => x.monthZeroIndex === currentTimeObj.month);
+        const remainingMonths = allMonthObjectsDuringThisYear.filter(x => x.monthZeroIndex > currentTimeObj.month -1);
 
         //Add days from prev month in case that will be rendered if this month doesn't start on a monday
-        var prevMonth = selectedDate.clone().subtract(1, 'month');
-        const isPrevMonthDifferentYear = prevMonth.year() !== selectedDate.year();
-        var mondayIndex = SWEDISH_WEEKDAY_NAMES.indexOf(days[0].veckodag);
-        if (isPrevMonthDifferentYear) { //requires extra API call
-            var previousMonthEndpoint = `${baseEndpoint}/${prevMonth.year()}`;
-            var prevMonthResponse = await request(previousMonthEndpoint);
-            var previousMonthData = prevMonthResponse.data;
-            var previousDays = previousMonthData.dagar.splice(previousMonthData.dagar.length - mondayIndex, mondayIndex);
-            days = previousDays.concat(days);
-        } else {
-            var prevMonthZeroIndex = currentRenderingMonthZeroIndex -1;
-            if (prevMonthZeroIndex > -1) {
-                var previousMonthData = allMonthObjectsDuringThisYear[prevMonthZeroIndex];
-                var previousDays = previousMonthData.days.splice(previousMonthData.days.length - mondayIndex, mondayIndex);
-                days = previousDays.concat(days);
-            }
-        }
+        // var prevMonth = currentTimeObj.dayJsObject.clone().subtract(1, 'month');
+        // const isPrevMonthDifferentYear = prevMonth.year() !== currentTimeObj.dayJsObject.year();
+        // var mondayIndex = SWEDISH_WEEKDAY_NAMES.indexOf(daysOfCurrentMonth[0].veckodag);
+        // if (isPrevMonthDifferentYear) { //requires extra API call
+        //     var previousMonthEndpoint = `${baseEndpoint}/${prevMonth.year()}`;
+        //     var prevMonthResponse = await request(previousMonthEndpoint);
+        //     var previousMonthData = prevMonthResponse.data;
+        //     var previousDays = previousMonthData.dagar.splice(previousMonthData.dagar.length - mondayIndex, mondayIndex);
+        //     daysOfCurrentMonth = previousDays.concat(daysOfCurrentMonth);
+        // } else {
+        //     var prevMonthZeroIndex = currentTimeObj.monthZeroIndex -1;
+        //     if (prevMonthZeroIndex > -1) {
+        //         var previousMonthData = allMonthObjectsDuringThisYear[prevMonth];
+        //         var previousDays = previousMonthData.days.splice(previousMonthData.daysOfCurrentMonth.length - mondayIndex, mondayIndex);
+        //         daysOfCurrentMonth = previousDays.concat(daysOfCurrentMonth);
+        //     }
+        // }
 
-        days = getExtendedArrayOfDays(days);
+        var selectedMonth = { days: data.dagar, monthName: currentTimeObj.dayJsObject.format('MMMM') };
+        
+        var todayFiltered = allDaysOfYear.filter(day => dayjs(day.datum).isToday());
+        var today = { 
+            weekday: d.format('dddd'), 
+            day: d.date(), 
+            week: d.week(), 
+            month: d.format('MMMM'),
+            ...todayFiltered
+        };
 
-        var selectedMonth = { days: data.dagar, monthName: selectedDate.format('MMMM') };
-        var today = { weekday: d.format('dddd'), day: d.date(), week: d.week(), month: d.format('MMMM') };
 
-        var todayExtra = days.find(d => dayjs(d.datum).isToday());
-        today = { ...today, ...todayExtra};
-
-        var nextYearObject = dayjs(`${year+1}-01-01`, "YYYY-MM-DD");
-        var startOfYear = dayjs(`${year}-01-01`, "YYYY-MM-DD").subtract(1, 'months');
-        var prevMonthMomentObject = dayjs(`${year}-${month}-01`, "YYYY-MM-DD").subtract(1, 'months');
-        var prevDateObj = prevMonthMomentObject;
-        var nextQueryString = `showCalendar?year=${nextYearObject.format('YYYY')}&month=${nextYearObject.format('MM')}`;
-        var prevQueryString = `showCalendar?year=${prevDateObj.format('YYYY')}&month=${prevDateObj.format('MM')}`;
-
-        res.render("index", {
+        const viewModel = {
             config: global.config,
-            days, //the selected month days plus perhaps some days from the prev month
             year,
-            weekNumbers: getUniqueWeekNumbersForArrayOfDays(days),
-            month: {
+            currentMonth: {
+                days: daysOfCurrentMonth, //the selected month days plus perhaps some days from the prev month
                 number: month,
-                name: dayjs(`${year}-${month}-01`, "YYYY-MM-DD").format('MMMM')
+                name: dayjs(`${year}-${month}-01`, "YYYY-MM-DD").format('MMMM'),
+                weekNumbers: getUniqueWeekNumbersForArrayOfDays(daysOfCurrentMonth),
             },
             today,
             selectedMonth,
             nextQueryString,
             prevQueryString,
             months: allMonthObjectsDuringThisYear,
-            remainingMonths: allMonthObjectsDuringThisYear.filter(x => x.zeroIndexedMonthNumber > month -1)
-        });
+            restOfYear: {
+                months: remainingMonths
+            }
+        };
+        console.log(JSON.stringify({ restOfYear: viewModel.restOfYear }));
+        res.render("index", viewModel);
     } catch (e) {
         console.log("ERROR", e);
         res.render("error", {
@@ -200,7 +221,7 @@ async function renderCalendarForMonth(yearString, monthStringWithLeadingZero, re
 }
 
 function getUniqueWeekNumbersForArrayOfDays(days) {
-    const weeks= days.map(x => x.vecka);
+    const weeks = days.map(x => x.vecka);
     const uniqueWeeks = [...new Set(weeks)];
     return uniqueWeeks;
 }
