593e10baca5cb25b81b552bdddf5a2a5429df0db
niklasHagner <niklas.hagner@outlook.com>
2023-01-14 18:38:10 +0100

    2023 themedays

--- scrapeThemeDays.js
+++ scrapeThemeDays.js
@@ -4,8 +4,7 @@ const { JSDOM } = jsdom;
 const fs = require('fs');
 
 /*
-Note about JSDOM
-    * they refuse to implement innertext, we'll have to use textContent instead
+JSDOM haven't implemented the innertext property, use textContent instead
 */
 
 const endPoint = 'https://temadagar.se/kalender/';
@@ -17,34 +16,11 @@ request(endPoint).then((axiosResponse) => {
         The page is not very structured, everything relevant lies flat inside one large parent.
 
         Steps
-        * to get all temadagar for one year - filter p tags between headline2022 and headline 2023
+        * to get all temadagar for one year - filter p tags between yearHeadline and headline 2023
         * each day is a paragraph where the first anchor is the date, and the other anchors are the themedays
     */
-    const headline2022 = Array.from(document.querySelectorAll("h2")).find(x => x.textContent === "Kalender med temadagar 2022");
-    const headline2023 = Array.from(document.querySelectorAll("h2")).find(x => x.textContent === "Kalender med temadagar 2023");
-    const root = headline2022.parentElement;
-
-    const elementArray = Array.from(root.childNodes);
-    const indexOf2022 = elementArray.indexOf(headline2022);
-    const indexOf2023 = elementArray.indexOf(headline2023);
-    const elementsWithin2022 = elementArray.slice(indexOf2022, indexOf2023);
-    const paragraphsWithin2022 = elementsWithin2022.filter(x => x.nodeName === "P");
-
-    const results = [];
-    paragraphsWithin2022.forEach((x) => { getThemeDaysForYear(x, results, "2022"); });
-    console.log("results.length", results.length);
-
-    const saveDate = new Date().toISOString().split('T')[0];
-
-    const data = JSON.stringify({fileScrapeDate: saveDate, daysScraped: results});
-    fs.writeFile(`temadagar.json`, data, (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log("JSON data is saved.");
-    });
-
-    function getThemeDaysForYear(possibleDayNode, resultArray, year) {
+    
+    function getThemeDaysForYear(possibleDayNode, year) {
         const anchorElements = Array.from(possibleDayNode.querySelectorAll("a"));
         if (anchorElements.length < 1) return;
 
@@ -52,20 +28,22 @@ request(endPoint).then((axiosResponse) => {
         if (themeDayElements.length < 1) return;
 
         let dayAndSweMonth = anchorElements[0].querySelector("b").textContent;
-        if (dayAndSweMonth) {
-            //dayname is something like "2 Maj"
-            const splitDayMonth = dayAndSweMonth.split(" ");
-            let dayInt = splitDayMonth[0];
-            let dayStr = `${dayInt}`;  
-            if (dayStr.length === 1) {
-                dayStr = "0" + dayStr;
-            }
-            const swedishMonth = splitDayMonth[1];
-            const montStr = mapSweMonthToStandardMonthNum(swedishMonth);
-            const yyyymmdd = `${year}-${montStr}-${dayStr}`;
-            const themeDays = themeDayElements.map(x => x.textContent);
-            resultArray.push({ date: yyyymmdd, themeDays });
+        if (!dayAndSweMonth) {
+            //error
+            return null;
         }
+        //dayname is something like "2 Maj"
+        const splitDayMonth = dayAndSweMonth.split(" ");
+        let dayInt = splitDayMonth[0];
+        let dayStr = `${dayInt}`;  
+        if (dayStr.length === 1) {
+            dayStr = "0" + dayStr;
+        }
+        const swedishMonth = splitDayMonth[1];
+        const montStr = mapSweMonthToStandardMonthNum(swedishMonth);
+        const yyyymmdd = `${year}-${montStr}-${dayStr}`;
+        const themeDays = themeDayElements.map(x => x.textContent);
+        return { date: yyyymmdd, themeDays };
     }
 
     function mapSweMonthToStandardMonthNum(string) {
@@ -83,4 +61,24 @@ request(endPoint).then((axiosResponse) => {
         if (string === "november") return "11";
         if (string === "december") return "12";
     }
+
+    const currentYearHeadline = Array.from(document.querySelectorAll("h2")).find(x => x.textContent === "Kalender med temadagar 2023");
+    const nextYearHeadline = Array.from(document.querySelectorAll("h2")).find(x => x.textContent === "Kalender med temadagar 2024");
+    const root = currentYearHeadline.parentElement;
+
+    const elementArray = Array.from(root.childNodes);
+    const elementsWithinYear = elementArray.slice(elementArray.indexOf(currentYearHeadline), elementArray.indexOf(nextYearHeadline));
+    const paragraphs = elementsWithinYear.filter(x => x.nodeName === "P");
+
+    const themeDays = paragraphs.map((para) => getThemeDaysForYear(para, "2023")).filter(x => x);
+    console.log("themeDays.length", themeDays.length);
+
+    const data = JSON.stringify({daysScraped: themeDays});
+    fs.writeFile(`temadagar.json`, data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+    });
+
 });
